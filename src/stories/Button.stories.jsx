a0f15c6582d722b2a3441d81d9ee14677bf38517
Utkarshk7 <80442043+Utkarshk7@users.noreply.github.com>
2022-05-08 01:37:29 -0400

    story book

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from './Button';
+import { Button } from "./Button";
 
+//👇 This default export determines where your story goes in the story list
 export default {
-  title: 'Example/Button',
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Example/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 };
 
@@ -15,22 +20,25 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: "Click Primary!",
 };
 
+//👇 Each story then reuses that template
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: "Button",
 };
 
+//👇 Each story then reuses that template
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  size: "large",
+  label: "Button",
 };
 
+//👇 Each story then reuses that template
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: "small",
+  label: "Button",
 };
